c82f0b6d36e59444b1b3dc229c8901d299353a0f
zhooson <861077046@qq.com>
2018-09-19 15:16:41 +0800

    fix react-transition-froup动画库使用

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react'
-import { Transition } from 'react-transition-group'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import './transitions.css' 
 
 class App extends Component {
@@ -7,7 +7,9 @@ class App extends Component {
 	constructor(props) {
     super(props);
     this.state = {
-    	show: true
+    	show: true,
+    	cssshow: true,
+    	list: []
     }
   }
 
@@ -17,11 +19,50 @@ class App extends Component {
 					<div className={`${this.state.show ? 'show' : 'hide'} animated`}>App</div>
 					<button onClick={this.toggleHandler}>toggle</button>
 					<br />
+					<br />
+					<br />
+					<br />
+					<br />
 
-					<Transition>
+					<CSSTransition
+					in={ this.state.cssshow }
+					timeout={ 300 }
+					classNames="fade"
+					unmountOnExit
+					onEntered={(el) => {el.style.color = 'red'}}
+					appear={ true }
+					>
 						<div>hello world</div>						
-					</Transition>
+					</CSSTransition>
 					<button onClick={this.cssTransionHandler}>cssTransionHandler</button>
+					<br />
+					<br />
+					<br />
+					<br />
+					<br />
+
+					<ul>
+						<TransitionGroup>
+							{
+	              this.state.list.map((item, index) => {
+	                  return (
+											<CSSTransition
+													key={index}
+													timeout={ 300 }
+													classNames="fade"
+													 unmountOnExit
+													onEntered={(el) => {el.style.color = 'red'}}
+													appear={ true }
+													>
+	                    	<li onClick={ this.delHandler.bind(this,index) }>{item}</li>
+	                    </CSSTransition>
+	                  )
+	              })
+	            }
+	           </TransitionGroup>
+					</ul>
+					<button onClick={this.additemHandler}>增加数据</button>
+
 
 
 				</Fragment>
@@ -35,7 +76,27 @@ class App extends Component {
 	}
 
 	cssTransionHandler=()=>{
+		this.setState({
+				cssshow: this.state.cssshow ? false : true
+		})
+	}
 
+	additemHandler=()=>{
+		this.setState((prevsState)=>{
+			return {
+				list: [...prevsState.list, 'item']
+			}
+		})
+	}
+
+	delHandler(index){
+		console.info('index',index)
+
+		const list = [...this.state.list];
+		list.splice(index,1)
+		this.setState(()=>({
+			list: list
+		}))
 	}
 
 }
