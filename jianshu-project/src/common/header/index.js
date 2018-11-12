7c336b6dc11b5cf451d57ef36cf84cc563ae1af6
zhooson <861077046@qq.com>
2018-11-12 22:17:23 +0800

    fix 简书header部分开发完毕

--- jianshu-project/src/common/header/index.js
+++ jianshu-project/src/common/header/index.js
@@ -1,4 +1,7 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
+import React from 'react'
+import { connect } from 'react-redux'
+
 import {
 	HeaderStyle,
 	Logo,
@@ -9,11 +12,14 @@ import {
 	Addtion,
 	Button
 } from './style'
+/**
+
+
 class Header extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			isFocused: false
+			// isFocused: false
 		}
 	}
 	render(){
@@ -27,13 +33,13 @@ class Header extends Component {
 					<NavItem className="right">
 						<i className="iconfont">&#xe636;</i>
 					</NavItem>
-					<SearchBox className={ this.state.isFocused?'focused':'' }>
+					<SearchBox className={ this.props.isFocused?'focused':'' }>
 						<Search 
 							className="focused-input"
-							onFocus={this.focusHandler}
-							onBlur={this.blurHandler}>
+							onFocus={this.props.focusHandler}
+							onBlur={this.props.blurHandler}>
 						</Search>
-						<i className="iconfont focused-iconfont">&#xe623;</i>
+						<i className="i confont focused-iconfont">&#xe623;</i>
 					</SearchBox>
 				</Nav>
 				<Addtion>
@@ -44,16 +50,69 @@ class Header extends Component {
 		)
 	}
 
-	focusHandler=(e)=>{
-		this.setState({
-			isFocused: true
-		})
-	}
+	// focusHandler=(e)=>{
+		// this.setState({
+		// 	isFocused: true
+		// })
+	// }
 
-	blurHandler=(e)=>{
-		this.setState({
-			isFocused: false
-		})
+	// blurHandler=(e)=>{
+		// this.setState({
+		// 	isFocused: false
+		// })
+	// }
+}
+*/ 
+
+const Header =(props) =>{
+	return (
+			<HeaderStyle>
+			<Logo />
+			<Nav>
+				<NavItem className="left active">首页</NavItem>
+				<NavItem className="left">下载App</NavItem>
+				<NavItem className="right">登陆</NavItem>
+				<NavItem className="right">
+					<i className="iconfont">&#xe636;</i>
+				</NavItem>
+				<SearchBox className={props.isFocused?'focused':'' }>
+					<Search 
+						className="focused-input"
+						onFocus={props.focusHandler}
+						onBlur={props.blurHandler}>
+					</Search>
+					<i className="i confont focused-iconfont">&#xe623;</i>
+				</SearchBox>
+			</Nav>
+			<Addtion>
+				<Button><i className="iconfont">&#xe6c5;</i>写文章</Button>
+				<Button className="write">登陆</Button>
+			</Addtion>
+		</HeaderStyle>
+	)
+}
+
+const mapStateToProps = (state) =>{
+	return {
+		isFocused: state.header.get('isFocused') // immutable 的写法
 	}
 }
-export default Header
+
+const mapDispatchToProps =(dispatch) =>{
+	return {
+		focusHandler(){
+			const action = {
+				type: 'SEARCH_FOCUS'
+			}
+			dispatch(action)
+		},
+		blurHandler(){
+			const action = {
+				type: 'SEARCH_BLUR'
+			}
+			dispatch(action)
+		}
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header)
