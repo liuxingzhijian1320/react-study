3ba8672f1e745fc03ffcbc4b82b1b5a24ec344aa
zhooson <861077046@qq.com>
2018-11-13 17:36:14 +0800

    fix 文章列表 redux

--- jianshu-project/src/components/List/List.js
+++ jianshu-project/src/components/List/List.js
@@ -1,4 +1,5 @@
 import React ,{ Component } from 'react'
+import { connect } from 'react-redux'
 import {
 	ListItem,
 	ListInfo,
@@ -10,24 +11,22 @@ class List extends Component {
 		return (
 			<div>
 				{
-					[1,2,34].map((item,index)=>{
+					this.props.articleList.map((item,index)=>{
 						return (
 							<ListItem key={index}>
-								<img className="img" alt='' src='//upload-images.jianshu.io/upload_images/1954956-f39159eec6da2c01?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240' />
+								<img className="img" alt='' src={item.get('img')} />
 								<ListInfo>
-									<h3 className='h3'>下班后，这3种副业能帮你赚得高收入！</h3>
-									<div className='info'>
-										在职场中，如果想获得比较高的收入会相对比较难。因为每个人都有对应的工作内容，有自己固定的职责。因此获得的报酬也是有一定限制的。所以，如果想快速增加收入，不妨发展自己的副业。那么，哪些副业更容易获得报酬呢？
-									</div>
+									<h3 className='h3'>{item.get('title')}</h3>
+									<div className='info'>{item.get('info')}</div>
 								</ListInfo>
 								<ListBottom>
-									<i className="name">流星值箭1320</i>
+									<i className="name">{item.get('name')}</i>
 									<i className="iconfont icon">&#xe606;</i>
-									<i className="value">23</i>
+									<i className="value">{item.get('msg')}</i>
 									<i className="iconfont icon">&#xe653;</i>
-									<i className="value">23</i>
+									<i className="value">{item.get('likes')}</i>
 									<i className="iconfont icon">&#xe601;</i>
-									<i className="value">23</i>
+									<i className="value">{item.get('money')}</i>
 								</ListBottom>
 							</ListItem>
 						)
@@ -39,4 +38,10 @@ class List extends Component {
 
 	}
 }
-export default List
+const mapStateToProps = (state) =>{
+	return {
+		articleList: state.home.get('articleList')
+	}
+}
+
+export default connect(mapStateToProps, null)(List)
--- jianshu-project/src/store/reducer/home.js
+++ jianshu-project/src/store/reducer/home.js
@@ -12,6 +12,38 @@ const defaultState = fromJS({
 			title:'故事',
 			img:'//upload.jianshu.io/collections/images/95/1.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64'
 		}
+	],
+	articleList:[
+		{
+			id:1,
+			title:'下班后，这3种副业能帮你赚得高收入！',
+			info:'在职场中，如果想获得比较高的收入会相对比较难。因为每个人都有对应的工作内容，有自己固定的职责。因此获得的报酬也是有一定限制的。所以，如果想快速增加收入，不妨发展自己的副业。那么，哪些副业更容易获得报酬呢？',
+			img:'//upload-images.jianshu.io/upload_images/1954956-f39159eec6da2c01?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240',
+			name:'流星值箭1320',
+			msg:10,
+			likes:100,
+			money: 33
+		},
+		{
+			id:2,
+			title:'下班后，这3种副业能帮你赚得高收入！',
+			info:'在职场中，如果想获得比较高的收入会相对比较难。因为每个人都有对应的工作内容，有自己固定的职责。因此获得的报酬也是有一定限制的。所以，如果想快速增加收入，不妨发展自己的副业。那么，哪些副业更容易获得报酬呢？',
+			img:'//upload-images.jianshu.io/upload_images/1954956-f39159eec6da2c01?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240',
+			name:'流星值箭1320',
+			msg:19,
+			likes:90,
+			money: 20
+		},
+		{
+			id:3,
+			title:'下班后，这3种副业能帮你赚得高收入！',
+			info:'在职场中，如果想获得比较高的收入会相对比较难。因为每个人都有对应的工作内容，有自己固定的职责。因此获得的报酬也是有一定限制的。所以，如果想快速增加收入，不妨发展自己的副业。那么，哪些副业更容易获得报酬呢？',
+			img:'//upload-images.jianshu.io/upload_images/1954956-f39159eec6da2c01?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240',
+			name:'流星值箭1320',
+			msg:100,
+			likes:130,
+			money: 13
+		}
 	]
 })
 
